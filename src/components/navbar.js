0c2f681a393b970dccde5aa9c444a03f1c75447e
Valerio Pinsone <valerio.pinsone@gmail.com>
2023-01-24 22:08:14 +0100

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+    return(
+        <nav className="navbar bg-body-tertiary bg-dark navbar-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                &nbsp;React Test
+                </a>
+            </div>
+        </nav>
+    );
+
+}
+export default Navbar;
